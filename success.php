c1082978210cd1061661a05d1fde3f88e6d4cb46
BunningVa <88389517+BunningVa@users.noreply.github.com>
2021-08-15 00:59:41 +0700

    Update success.php

--- success.php
+++ success.php
@@ -23,26 +23,7 @@
 ?>
    
    <?php require_once 'includes/footer.php';?>
-    <!-- This prints out values that weres passed to the action page using method="get" -->
-    <!-- <div class="card" style="width: 18rem;">
-        <div class="card-body">
-            <h5 class="card-title">
-                <?php //echo $_GET['firstname']. ' '. $_GET['lastname'];?>
-            </h5>
-            <h6 class="card-subtitle mb-2 text-muted">
-                <?php// echo $_GET['specialty'];?>
-            </h6>
-            <p class="card-text">
-                Date Of Birth: <?php //echo $_GET['dob'];?>
-            </p>
-            <p class="card-text">
-                Email Address: <?php// echo $_GET['email'];?>
-            </p>
-            <p class="card-text">
-                Contact Number<?php //echo $_GET['phone'];?>
-            </p>    
-        </div>
-    </div> -->
+
     <div class="card" style="width: 18rem;">
         <div class="card-body">
             <h5 class="card-title">
@@ -64,3 +45,25 @@
     </div>
 <?php         }
     }?>
+
+
+    <!-- This prints out values that weres passed to the action page using method="get" -->
+    <!-- <div class="card" style="width: 18rem;">
+        <div class="card-body">
+            <h5 class="card-title">
+            <?php //echo $_GET['firstname']. ' '. $_GET['lastname'];?>
+            </h5>
+            <h6 class="card-subtitle mb-2 text-muted">
+                <?php// echo $_GET['specialty'];?>
+            </h6>
+            <p class="card-text">
+                Date Of Birth: <?php //echo $_GET['dob'];?>
+            </p>
+            <p class="card-text">
+                Email Address: <?php// echo $_GET['email'];?>
+            </p>
+            <p class="card-text">
+                Contact Number<?php //echo $_GET['phone'];?>
+            </p>    
+        </div>
+    </div> -->
